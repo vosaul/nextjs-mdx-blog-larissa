9e8541e58ac88200a80c5e55b127813faa2ce215
Volodymyr Sauliak <v986963@gmail.com>
2023-12-18 13:30:48 +0200

    styles

--- src/app/exhibitions/[slug]/page.tsx
+++ src/app/exhibitions/[slug]/page.tsx
@@ -68,7 +68,7 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
       <time className="my-4 block text-sm text-zinc-400" dateTime={post.date}>
         {format(parseISO(post.date), 'LLLL d, yyyy')}
       </time>
-      <article className="prose dark:prose-invert">
+      <article className="prose dark:prose-invert lg: prose-xl mx-auto">
         <MDXContent components={mdxComponents} />
       </article>
     </div>
