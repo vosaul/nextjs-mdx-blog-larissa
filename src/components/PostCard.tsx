8fdbf9df396f64a22c66e280ae8c97aa1c388287
Volodymyr Sauliak <v986963@gmail.com>
2024-04-26 21:33:49 +0300

    some styles

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -6,7 +6,8 @@ export function PostCard(post: Post) {
   return (
     <>
       {post.image ? (
-        <article className="flex flex-col justify-start border border-solid rounded-lg w-[300px]  ">
+        <article className="flex flex-col justify-start border border-solid rounded-lg hover:border-gray-300 hover:shadow-md shadow-gray-400 w-[300px] hover:bg-orange-50 ">
+          <Link className="link" href={post.url}>
             <div className="max-h-[200px] overflow-y-hidden">
               <Image
                 className='block self-start '
@@ -23,33 +24,33 @@ export function PostCard(post: Post) {
                 </time>
               </div>
               <h3 className="mt-3 text-lg font-semibold leading-6">
-                <Link className="link" href={post.url}>
-                  {post.title}
-                </Link>
+                {post.title}
               </h3>
               <p className="mt-5 line-clamp-3 text-sm leading-6">
                 {post.description}
               </p>
             </div>
+          </Link>
+
         </article>
       ) : (
         <article className="flex flex-col items-start justify-between">
-          <div className="flex items-center gap-x-4 text-xs">
-            <time dateTime={post.date}>
-              {format(parseISO(post.date), 'dd.MM.yyyy')}
-            </time>
-          </div>
-          <div className="group relative">
-            <h3 className="mt-3 text-lg font-semibold leading-6">
-              <Link className="link" href={post.url}>
+          <Link className="link" href={post.url}>
+            <div className="flex items-center gap-x-4 text-xs">
+              <time dateTime={post.date}>
+                {format(parseISO(post.date), 'dd.MM.yyyy')}
+              </time>
+            </div>
+            <div className="group relative">
+              <h3 className="mt-3 text-lg font-semibold leading-6">
                 <span className="absolute inset-0" />
                 {post.title}
-              </Link>
-            </h3>
-            <p className="mt-5 line-clamp-3 text-sm leading-6">
-              {post.description}
-            </p>
-          </div>
+              </h3>
+              <p className="mt-5 line-clamp-3 text-sm leading-6">
+                {post.description}
+              </p>
+            </div>
+          </Link>
         </article>
       )}
     </>
